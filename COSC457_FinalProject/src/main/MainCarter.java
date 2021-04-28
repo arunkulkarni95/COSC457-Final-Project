7991a97decc6a48068aa0339b122658aec8492a0
CarterMontgomery <73906654+CarterMontgomery@users.noreply.github.com>
2021-04-27 20:14:03 -0400

    Add files via upload
    Added Update and Delete

--- COSC457_FinalProject/src/main/MainCarter.java
+++ COSC457_FinalProject/src/main/MainCarter.java
@@ -3,8 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package main;
-//import com.mysql.cj.jdbc.result.ResultSetMetaData;
+package dbproject;
+import com.mysql.cj.jdbc.result.ResultSetMetaData;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -61,6 +61,117 @@ public class MainCarter {
         return success;
     }
     
+    public static boolean update(Connection con, String table, List<String> newElem) {
+        boolean success = false;
+        try {
+            Statement switch_tables = con.createStatement();
+        
+            switch_tables.executeQuery("use "+"mydb");
+            ResultSet rs = switch_tables.executeQuery("SELECT * FROM "+table+" WHERE 0=1");
+            String updateStr = "";
+            String primaryKey = "WHERE ";
+            
+            ResultSet primaryKeys = (con.getMetaData().getPrimaryKeys(null,null,table));
+            List<String> pKs = new ArrayList<String>();
+            //Get primary keys
+            while(primaryKeys.next()) {
+                String pkColumnName = primaryKeys.getString("COLUMN_NAME");
+                //Integer pkPosition = primaryKeys.getInt("KEY_SEQ");
+                //System.out.println(""+pkColumnName+" is the "+pkPosition+". column of the primary key of the table "+table);
+                //pkColumnSet.add(pkColumnName);
+                pKs.add(pkColumnName);
+            }
+            int pKsFound = 0;
+            java.sql.ResultSetMetaData mD = rs.getMetaData();
+            for (int i=1;i<mD.getColumnCount()+1;i++) {
+                updateStr = updateStr + mD.getColumnName(i)+"="+"\'"+newElem.get(i-1)+"\'";
+                if (i!=mD.getColumnCount()) {
+                    updateStr = updateStr + ", ";
+                }
+                for (int j=0;j<pKs.size();j++) {
+                    if (mD.getColumnName(i).equalsIgnoreCase(pKs.get(j))){
+                        if (pKsFound == 0) {
+                        primaryKey = primaryKey + pKs.get(j)+"=\'"+newElem.get(i-1)+"\'";
+                        } else {
+                            primaryKey = primaryKey + " AND "+pKs.get(j)+"=\'"+newElem.get(i-1)+"\'";
+                        }
+                        pKsFound += 1;
+                    }
+                }
+                //System.out.println(mD.getColumnType(i));
+            }
+            //System.out.println(updateStr);
+            //System.out.println(primaryKey);
+            String query = "UPDATE "+table+" \nSET "+updateStr+"\n"+primaryKey+";";
+            //System.out.println(query);
+//String stmt = "";
+            //PreparedStatement upd = con.prepareStatement(stmt);
+            //upd.execute();
+            PreparedStatement upd = con.prepareStatement(query);
+            upd.execute();
+            success = true;
+        }
+        catch (SQLException ex) {
+            Logger.getLogger(MainCarter.class.getName()).log(Level.SEVERE, null, ex);
+            success = false;
+        }
+        return success;
+    }
+    public static boolean delete(Connection con,String table, List<String> pKeys) {
+        boolean success = false;
+        try {
+            Statement switch_tables = con.createStatement();
+        
+            switch_tables.executeQuery("use "+"mydb");
+            String query = "DELETE FROM "+table +" WHERE ";
+            String pKeysPart = "";
+            ResultSet primaryKeys = (con.getMetaData().getPrimaryKeys(null,null,table));
+            List<String> pKs = new ArrayList<String>();
+            //Get primary keys
+            while(primaryKeys.next()) {
+                String pkColumnName = primaryKeys.getString("COLUMN_NAME");
+                pKs.add(pkColumnName);
+            }
+            int pKsFound = 0;
+            ResultSet rs = switch_tables.executeQuery("SELECT * FROM "+table);
+            java.sql.ResultSetMetaData mD = rs.getMetaData();
+            for (int i=1;i<mD.getColumnCount()+1;i++) {
+                for (int j=0;j<pKs.size();j++) {
+                    if (mD.getColumnName(i).equalsIgnoreCase(pKs.get(j))){
+                        if (pKsFound == 0) {
+                            pKeysPart = pKeysPart + pKs.get(j)+"=\'"+pKeys.get(pKsFound)+"\'";
+                            success = true;
+
+                        } else {
+                            if (pKeys.size() < j) {
+                                pKeysPart = pKeysPart + " AND "+pKs.get(j)+"=\'"+pKeys.get(pKsFound)+"\'";
+                                success = true;
+                            } else {
+                                System.out.println("Table has more primary keys than provided.");
+                                success = false;
+                            }
+                        }
+                        pKsFound += 1;
+                    }
+                }
+                
+            }
+            if (success) {
+                query = query + pKeysPart + ";";
+                //System.out.println(query);
+                PreparedStatement del = con.prepareStatement(query);
+                del.execute();
+                success = true;
+            }
+            
+
+        } catch (SQLException ex) {
+            Logger.getLogger(MainCarter.class.getName()).log(Level.SEVERE, null, ex);
+            success = false;
+        }
+        return success;
+    }
+    
     public static List<List<String>> convertRSToString(ResultSet rs) {
         List<List<String>> retString = new ArrayList<List<String>>();
 
@@ -68,6 +179,7 @@ public class MainCarter {
             java.sql.ResultSetMetaData rMD = rs.getMetaData();
             final int columnCount = rMD.getColumnCount();
             System.out.println(columnCount);
+            
             while(rs.next()) {
                 List<String> values = new ArrayList<String>();
                 for (int i=1;i<columnCount+1;i++) {
