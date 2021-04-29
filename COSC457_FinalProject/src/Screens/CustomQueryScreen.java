c9eb0ec01e95a982507fd2def75582380584493c
arunkulkarni95 <arunkulkarni95@hotmail.com>
2021-04-28 21:53:22 -0400

    Update CustomQueryScreen.java

--- COSC457_FinalProject/src/Screens/CustomQueryScreen.java
+++ COSC457_FinalProject/src/Screens/CustomQueryScreen.java
@@ -100,7 +100,6 @@ public class CustomQueryScreen extends JFrame{
                     JOptionPane.showMessageDialog(null, f.getMessage());
                 }
             }
-        }
-        
+        }       
     }
 }
